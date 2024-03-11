72a5ef763dffd5bdc8623e2a20a56e28581e5bab
Gülsüm <gulsumerd38@gmail.com>
2024-03-11 20:23:51 +0100

    Cute AND works!!

---
+++ js/index.js
@@ -0,0 +1,34 @@
+const app = Vue.createApp({
+
+  data(){
+    return {
+      number1: null,
+      number2: null,
+      result: null,
+      operator: null
+    };
+  },
+
+  methods: {
+    calculate(){
+      switch(this.operator){
+        case 'add':
+          this.result = this.number1 + this.number2;
+          break;
+        case 'subtract':
+          this.result = this.number1 - this.number2;
+          break;
+          case 'multiply':
+            this.result = this.number1 * this.number2;
+            break;
+          case 'divide':
+              this.result = this.number1 / this.number2;
+          break;
+          default:
+            this.result = 'Invalid Operation!!';
+      }
+    }
+  },
+});
+
+app.mount('#app');
