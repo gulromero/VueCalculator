a642f124922ed0234aed3557bff4db75d8399485
Gülsüm <gulsumerd38@gmail.com>
2024-03-14 15:01:44 +0100

    ID added for all the HTML elements. (:D)

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const app = Vue.createApp({
-
-  data(){
+  data() {
     return {
       number1: null,
       number2: null,
@@ -8,27 +7,26 @@ const app = Vue.createApp({
       operator: null
     };
   },
-
   methods: {
-    calculate(){
-      switch(this.operator){
+    calculate() {
+      switch(this.operator) {
         case 'add':
           this.result = this.number1 + this.number2;
           break;
         case 'subtract':
           this.result = this.number1 - this.number2;
           break;
-          case 'multiply':
-            this.result = this.number1 * this.number2;
-            break;
-          case 'divide':
-              this.result = this.number1 / this.number2;
+        case 'multiply':
+          this.result = this.number1 * this.number2;
           break;
-          default:
-            this.result = 'Invalid Operation!!';
+        case 'divide':
+          this.result = this.number1 / this.number2;
+          break;
+        default:
+          this.result = 'Invalid Operation!!';
       }
     }
-  },
+  }
 });
 
 app.mount('#app');
